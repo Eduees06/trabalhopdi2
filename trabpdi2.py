5c095ce592e96786d0340a1de1823b850b24d16f
Eduees06 <102429788+Eduees06@users.noreply.github.com>
2024-09-04 00:05:11 -0300

    finalizado

--- trabpdi2.py
+++ trabpdi2.py
@@ -1,37 +1,48 @@
-import cv2
 import numpy as np
 from skimage import io, img_as_float
+import cv2
 import matplotlib.pyplot as plt
 
-def high_boost_filter(image, k, filter_size=5):
-    # Converter a imagem para float
-    image = img_as_float(image)
+def kernel_gaussiano(tamanho, sigma=1):
+    eixo = np.linspace(-(tamanho // 2), tamanho // 2, tamanho)
+    kernel_1d = np.exp(-np.square(eixo) / (2 * np.square(sigma)))
+    kernel_1d /= np.sum(kernel_1d)
+    kernel_2d = np.outer(kernel_1d, kernel_1d)
+    return kernel_2d / kernel_2d.sum()
+
+def aplicar_filtro(imagem, kernel):
+    imagem_filtrada = cv2.filter2D(imagem, -1, kernel)
+    return imagem_filtrada
+
+def filtro_high_boost(imagem, k, tamanho_filtro=5, sigma=1):
+    imagem = img_as_float(imagem)
     
-    # Aplicar filtro passa-baixa usando um filtro gaussiano
-    low_pass = cv2.GaussianBlur(image, (filter_size, filter_size), 0)
+    # Criar e aplicar o filtro gaussiano
+    gauss_kernel = kernel_gaussiano(tamanho_filtro, sigma)
+    passa_baixa = aplicar_filtro(imagem, gauss_kernel)
     
     # High-boost
-    high_boost = image + k * (image - low_pass)
+    high_boost = imagem + k * (imagem - passa_baixa)
     
     return np.clip(high_boost, 0, 1)
 
 # Carregar a imagem
-image_path = 'D:/TrabPDI/path_to_image.jpg'
-image = io.imread(image_path, as_gray=True)
+caminho_imagem = 'D:/TrabPDI/path_to_image.jpg'
+imagem = io.imread(caminho_imagem, as_gray=True)
 
 # Aplicar o filtro high-boost
-k_values = [1, 1.5, 2, 2.5]
-filtered_images = [high_boost_filter(image, k) for k in k_values]
+valores_k = [5, 30, 50, 100]
+imagens_filtradas = [filtro_high_boost(imagem, k) for k in valores_k]
 
 # Exibir as imagens
-fig, axes = plt.subplots(1, len(k_values) + 1, figsize=(15, 5))
-axes[0].imshow(image, cmap='gray')
-axes[0].set_title('Original')
-axes[0].axis('off')
+fig, eixos = plt.subplots(1, len(valores_k) + 1, figsize=(15, 5))
+eixos[0].imshow(imagem, cmap='gray')
+eixos[0].set_title('Original')
+eixos[0].axis('off')
 
-for i, (k, img) in enumerate(zip(k_values, filtered_images)):
-    axes[i + 1].imshow(img, cmap='gray')
-    axes[i + 1].set_title(f'k = {k}')
-    axes[i + 1].axis('off')
+for i, (k, img) in enumerate(zip(valores_k, imagens_filtradas)):
+    eixos[i + 1].imshow(img, cmap='gray')
+    eixos[i + 1].set_title(f'k = {k}')
+    eixos[i + 1].axis('off')
 
 plt.show()
